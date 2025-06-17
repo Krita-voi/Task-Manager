b33fb95e47a25dc0cf134fccc03a7aef9b4e090f
Krita-voi <kritagyamaharjan794@gmail.com>
2025-06-17 20:21:39 +0545

    task manager

---
+++ TaskManager.js
@@ -0,0 +1,23 @@
+const buttons = document.querySelectorAll('.nav-btn');
+const secitons = document.querySelectorAll('.page');
+
+buttons.forEach(button => {
+    button.addEventListener('click',()=>{
+        const targetId = button.getAttribute('data-target');
+        
+        //remove ative class from all buttons
+        buttons.forEach(btn => btn.classList.remove('active'));
+
+        //add active to clicked button
+        button.classList.add('active');
+
+        //hide all secitons
+        secitons.forEach(secition => secitons.classList.add('hidden'));
+
+        //show target section
+        const targetSection = document.getElementById(targetId);
+        if(targetSection){
+            targetSection.classList.remove('hidden');
+        }
+    });
+});
